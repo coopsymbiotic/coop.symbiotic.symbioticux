f3a8a1a5175de2e691cf0f9b54c91b44b15b7aa9
Mathieu Lutfy <mathieu@bidon.ca>
2014-09-20 16:17:37 -0400

    Contribution forms: rename billing labels.

--- CRM/Symbioticux/Contribute/Form/Contribution/Main.php
+++ CRM/Symbioticux/Contribute/Form/Contribution/Main.php
@@ -2,6 +2,30 @@
 
 class CRM_Symbioticux_Contribute_Form_Contribution_Main {
   function buildForm(&$form) {
+    // Change the label on the "name" fields, and make mandatory
+    // (they are mandatory for CC transactions, but not shown as such in the form by default
+    // because of the 'pay later' option)
+    $fields = array('billing_first_name' => ts('First Name'), 'billing_last_name' => ts('Last Name'), 'billing_individual_prefix' => ts('Title'));
+
+    foreach ($fields as $field => $title) {
+      if ($form->elementExists($field)) {
+        $e =& $form->getElement($field);
+        $e->setLabel($title);
+        $form->addRule($field, ts('%1 is a required field.', array(1 => $title)), 'required');
+      }
+    }
+
+    $fields = array('billing_street_address-5', 'billing_city-5', 'billing_country_id-5', 'billing_state_province_id-5', 'billing_postal_code-5');
+
+    foreach ($fields as $field) {
+      if ($form->elementExists($field)) {
+        $e =& $form->getElement($field);
+        $title = $e->getLabel();
+        $form->addRule($field, ts('%1 is a required field.', array(1 => $title)), 'required');
+      }
+    }
+
+
     // Preview & demo sites should encourage to use a test CC number
     // otherwise users do not know what CC to use, and sometimes use their own.
     if (CRM_Utils_Array::value('action', $_REQUEST) == 'preview' || $GLOBALS['symbioticux_demo']) {
