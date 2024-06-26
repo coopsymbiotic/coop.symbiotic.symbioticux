11cdd039f6a15b4838b312738185307f6aa67f5b
Mathieu Lu <mathieu@symbiotic.coop>
2024-06-26 13:00:12 -0400

    Remove opiniated Contribution Form tweaks (ex: required billing fields), and upgrade civix

--- CRM/Symbioticux/Contribute/Form/Contribution/Main.php
+++ CRM/Symbioticux/Contribute/Form/Contribution/Main.php
@@ -15,33 +15,6 @@ class CRM_Symbioticux_Contribute_Form_Contribution_Main {
       }
     }
 
-    // Make billing fields mandatory, except the postcode if Stripe is enabled
-    // (in some countries, the postcode will not be required by Stripe).
-    $is_stripe = false;
-    $fields = ['billing_street_address-5', 'billing_city-5', 'billing_country_id-5', 'billing_state_province_id-5'];
-
-    foreach ($form->_paymentProcessors as $pp) {
-      if ($pp['class_name'] == 'Payment_Stripe') {
-        $is_stripe = true;
-      }
-    }
-
-    if (!$is_stripe) {
-      $fields[] = 'billing_postal_code-5';
-    }
-
-    foreach ($fields as $field) {
-      if ($form->elementExists($field)) {
-        $e =& $form->getElement($field);
-        $title = $e->getLabel();
-        $form->addRule($field, ts('%1 is a required field.', array(1 => $title)), 'required');
-      }
-    }
-
-    CRM_Core_Region::instance('page-body')->add(array(
-      'template' => 'CRM/Symbioticux/Contribute/Form/Contribution/Main.validate.tpl',
-    ));
-
     // Preview & demo sites should encourage to use a test CC number
     // otherwise users do not know what CC to use, and sometimes use their own.
     if (CRM_Utils_Array::value('action', $_REQUEST) == 'preview' || !empty($GLOBALS['symbioticux_demo']) || (isset($form->_paymentProcessors[1]) && CRM_Utils_Array::value('name', $form->_paymentProcessors[1]) == 'Dummy')) {
@@ -54,26 +27,12 @@ class CRM_Symbioticux_Contribute_Form_Contribution_Main {
                  cj(this).val('4111111111111111').trigger('change');
                  cj('#cvv2').val('123').trigger('change');
                  cj('#credit_card_exp_date_m').val('5').trigger('change');
-                 cj('#credit_card_exp_date_Y').val('2025').trigger('change');
-
+                 cj('#credit_card_exp_date_Y').val('2027').trigger('change');
                  cj('.symbioticux-billing-explain-cc').prependTo('.credit_card_info-section').slideDown();
                }
              });";
 
       CRM_Core_Resources::singleton()->addScript($js);
     }
-
-    CRM_Core_Region::instance('page-body')->add(array(
-      'template' => 'CRM/Symbioticux/Contribute/Form/Contribution/Main.hide-other-amount.tpl',
-    ));
-
-    // Remove some CSS classes on the 'submit' button.
-    $js = "cj('.crm-submit-buttons .crm-form-submit').addClass('btn btn-primary').removeClass('crm-form-submit').parent().removeClass('crm-button');";
-    CRM_Core_Resources::singleton()->addScript($js);
-
-    // Fix the translation of the "Credit Card Information" block
-    // c.f. templates/CRM/Core/BillingBlock.tpl
-    $smarty = CRM_Core_Smarty::singleton();
-    $smarty->assign('paymentTypeLabel', ts('Credit Card Information'));
   }
 }
--- templates/CRM/Symbioticux/Contribute/Form/Contribution/Main.hide-other-amount.tpl
+++
@@ -1,9 +0,0 @@
-{literal}
-<script type="text/javascript" >
-cj(function($) {
-  if ($('.other_amount-section').size() > 0) {
-    $('.contribution_amount-content .price-set-row:last-child').hide();
-  }
-});
-</script>
-{/literal}
